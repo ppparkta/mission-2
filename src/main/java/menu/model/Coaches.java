534e448bb4a52163a7c49ee8ce4c099f7afead43
ppparkta <pppppark2@gmail.com>
2024-12-09 21:44:14 +0900

    feat(Coaches): 코치 목록 유효성 검사
    추가 내역
    - 함께 밥 먹을 수 있는 인원 유효성검사

---
+++ src/main/java/menu/model/Coaches.java
@@ -0,0 +1,15 @@
+package menu.model;
+
+import java.util.List;
+import menu.util.ExceptionMessage;
+
+public class Coaches {
+    private final List<Coach> coaches;
+
+    public Coaches(List<Coach> coaches) {
+        if (coaches.size() < 2 || coaches.size() > 5) {
+            throw new IllegalArgumentException(ExceptionMessage.COACHES_SIZE_ERROR.getMessage());
+        }
+        this.coaches = coaches;
+    }
+}
