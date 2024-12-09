b9db39610f8e8ef630c00f29141409ed1cff2f96
ppparkta <pppppark2@gmail.com>
2024-12-09 21:41:38 +0900

    feat(Coach): 코치 도메인 유효성 검사
    추가 내역
    - 코치 이름 길이 유효성검사
    - 코치가 못 먹는 음식 사이즈 유효성 검사
    - 코치의 한 주 메뉴 중복 검사

---
+++ src/main/java/menu/model/Coach.java
@@ -0,0 +1,49 @@
+package menu.model;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import menu.util.ExceptionMessage;
+
+public class Coach {
+    private final String name;
+    private final Set<String> pickyMenus = new HashSet<>();
+    private final Set<String> weeklyMenus = new HashSet<>();
+
+    public Coach(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    public void addPickyMenu(List<String> menus) {
+        if (menus == null) {
+            return;
+        }
+        if (menus.size() > 2) {
+            throw new IllegalArgumentException(ExceptionMessage.PICKY_MENU_SIZE_ERROR.getMessage());
+        }
+        for (String menu : menus) {
+            validatePickyMenu(menu);
+            pickyMenus.add(menu);
+        }
+    }
+
+    public void addWeekMenu(String menu) {
+        if (weeklyMenus.contains(menu)) {
+            throw new IllegalArgumentException(ExceptionMessage.WEEKLY_MENU_DUPLICATED_ERROR.getMessage());
+        }
+        weeklyMenus.add(menu);
+    }
+
+    private void validatePickyMenu(String menu) {
+        if (pickyMenus.contains(menu)) {
+            throw new IllegalArgumentException(ExceptionMessage.PICKY_MENU_DUPLICATED_ERROR.getMessage());
+        }
+    }
+
+    private void validate(String name) {
+        if (name.length() < 2 || name.length() > 4) {
+            throw new IllegalArgumentException(ExceptionMessage.NAME_LENGTH_ERROR.getMessage());
+        }
+    }
+}
